414d530898da0e079102d8eac1fb57fd3de74c88
Naruson Banjongkan <narusonmtrs@hotmail.com>
2023-03-22 18:35:30 +0700

    picture cluster

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,6 +5,7 @@ const DB_NAME = 'scrumification'; // New database name
 const uri = `mongodb+srv://admin:root@cluster0.bjaozha.mongodb.net/${DB_NAME}`;
 const client = new MongoClient(uri);
 const dbCollection = client.db(DB_NAME).collection('tasks');
+const ObjectId = require('mongodb').ObjectId;
 
 // ====== for schema example ====== //
 const accountModel = require('../models/Account');
@@ -31,6 +32,71 @@ router.get('/', async(req, res) => {
             "status": "ok",
             "message": "Get tasks successfully",
             "tasks": tasks
+        });
+    } catch (error) {
+        res.status(400);
+        return res.json({
+            status: false,
+            data: "error",
+            errorDetail: error.toString(),
+        });
+    }
+});
+
+router.get('/:taskId', async(req, res) => {
+    try {
+        const client = new MongoClient(uri);
+        await client.connect();
+        const o_id = new ObjectId(req.params.taskId);
+        let task = await dbCollection.findOne({ _id: o_id });
+        await client.close();
+
+        res.status(200).send({
+            "status": "ok",
+            "message": "Get task successfully",
+            "task": task
+        });
+    } catch (error) {
+        res.status(400);
+        return res.json({
+            status: false,
+            data: "error",
+            errorDetail: error.toString(),
+        });
+    }
+});
+
+router.post('/cluster', async(req, res) => {
+    try {
+
+        //receive
+        // body: clusterId,taskId,point createdAt
+
+        const client = new MongoClient(uri);
+        await client.connect();
+        const t_id = new ObjectId(req.body.taskId);
+        const c_id = new ObjectId(req.body.clusterId);
+        let task = await dbCollection.findOne({ _id: t_id });
+
+        const [year, month, day] = req.body.createdAt.split("-");
+        const formattedDate = `${day}/${month}/${year}`;
+
+
+        const newTaskTransaction = {
+            _id: new ObjectId(),
+            task_id: t_id,
+            name: task.name,
+            point: req.body.point,
+            created_at: formattedDate
+        };
+
+        let updateCluster = await client.db(DB_NAME).collection('clusters').updateOne({ _id: c_id }, { $push: { task_transactions: newTaskTransaction } });
+        await client.close();
+
+        res.status(200).send({
+            "status": "ok",
+            "message": "Check task successfully",
+            "cluster": updateCluster,
         });
     } catch (error) {
         res.status(400);
